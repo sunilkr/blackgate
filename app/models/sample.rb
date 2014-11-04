7c9e46ae69fc4ea10f53019c55b406eea7a4be86
Sangey Wangchuck <sangey@sungkyop.org>
2014-11-04 10:01:32 +0530

    Added refernces for C&C and Container

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -4,6 +4,13 @@ class Sample < ActiveRecord::Base
   has_one :data, class_name: "SampleData"
   has_many :names, class_name: "SampleName", inverse_of: :sample
   has_many :incidents, through: :names, inverse_of: :samples
+  has_and_belongs_to_many :containers,
+                          class_name: "Sample",
+                          join_table: :containers
+  has_many :droppers, through: :containers
+  has_many :drops, through: :containers, foreign_key: :container_id
+  has_many :cnc_traffics, inverse_of: :sample
+  has_many :cncs, through: :cnc_traffics, class_name: "CommandAndControl", inverse_of: :samples
 
   serialize :categories, Array
 
